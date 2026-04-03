80e4b825bc0461fb9368914185ad88ac8e84ab56
Yeint Herp <yeint_herp@proton.me>
2026-04-03 11:49:31 +0200

    feat: Enter protected mode and print hello world

--- boot/stage2/stage2.asm
+++ boot/stage2/stage2.asm
@@ -1,6 +1,69 @@
+org 0x500
 
-mov al, 'H'
-mov ah, 0xe
-xor bx, bx
-int 0x10
-jmp $
+    mov bp, sp
+    sub bp, 6
+
+    ; prepare the gdtr on the stack.
+    mov word [bp], gdt_end - gdt_start - 1
+    mov dword [bp + 2], gdt_start
+
+    ; clear IF and load the global descriptor table.
+    cli
+    lgdt [bp]
+
+    ; set the protected mode bit.
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax
+
+    ; far jump to flush pipeline and load CS with code selector.
+    jmp 0x8:pm_entry
+
+use32
+
+pm_entry:
+    ; load data segments.
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    ; load stack segment and prepare stack.
+    mov ax, 0x18
+    mov ss, ax
+    mov esp, 0x7fffe
+
+    ; fast a20 gate enable.
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    jmp kernel_main
+
+gdt_start:
+    dq 0x0
+
+    dw 0xffff
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+    dw 0xffff
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+    dw 0x0000
+    dw 0x0000
+    db 0x00
+    db 10010110b
+    db 11000000b
+    db 0x00
+gdt_end:
+
+include 'c.asm'
