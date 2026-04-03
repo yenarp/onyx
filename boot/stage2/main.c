b4bcfdb9e9475289a3e7c5606162f2237a3efd2c
Yeint Herp <211648276+Yeint-herp@users.noreply.github.com>
2026-04-03 15:01:46 +0200

    feat: Replace temporary vga hello world with serial hello world

--- boot/stage2/main.c
+++ boot/stage2/main.c
@@ -1,26 +1,11 @@
-#define VGA_ADDR  0xb8000
-#define VGA_COLS  80
-#define VGA_ROWS  25
-
-static unsigned short *const vga = (unsigned short *)VGA_ADDR;
-
-#define vga_entry(c, col) ((c) | ((col) << 8))
-
-static void clear_screen(unsigned char color) {
-    for (unsigned i = 0; i < VGA_COLS * VGA_ROWS; i++)
-        vga[i] = vga_entry(' ', color);
-}
-
-static void print(const char *str, unsigned row, unsigned col, unsigned char color) {
-    unsigned offset = row * VGA_COLS + col;
-    for (unsigned i = 0; str[i] != '\0'; i++)
-        vga[offset + i] = vga_entry(str[i], color);
-}
+#include "dev/serial.h"
 
 void kernel_main(void) {
-    clear_screen(0x7);
-    print("Hello, World!", 0, 0, 0xf);
+    SerialPort com1;
+
+    serial_init(&com1, 0x3F8, SERIAL_BAUD_115200);
+    serial_puts(&com1, "Hello World from serial!\n");
 
     for (;;)
-        __asm__ volatile ("hlt");
+        __asm__ volatile("hlt");
 }
