b472c432087fb3152f98a2a2710976e18e0915d2
Yeint Herp <211648276+Yeint-herp@users.noreply.github.com>
2026-04-03 14:52:07 +0200

    fix: Force clang backend to target i386

--- mk/cc.mk
+++ mk/cc.mk
@@ -4,7 +4,7 @@ OBJCOPY ?= llvm-objcopy
 CC_TARGET := --target=i386-none-elf -std=gnu23
 CC_FSTAND := -ffreestanding -nostdlib -fno-builtin -masm=intel
 CC_HARDEN := -fno-stack-protector -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables
-CC_CODEGEN  := -mno-sse -mno-sse2 -mno-mmx -mno-80387 -fno-pie -fno-pic
+CC_CODEGEN  := -march=i386 -mno-sse -mno-sse2 -mno-mmx -mno-80387 -fno-pie -fno-pic
 CC_WARNINGS := -Wall -Wextra -Wpedantic -Werror=implicit-function-declaration -Werror=return-type
 
 CC_MODE_FLAGS ?=
