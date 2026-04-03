98cd016ba6226dd34004baa82b232c53d221b2fe
Yeint Herp <211648276+Yeint-herp@users.noreply.github.com>
2026-04-03 15:43:47 +0200

    build(bootloader): Improve build system

--- boot/module.mk
+++ boot/module.mk
@@ -4,13 +4,13 @@ STAGE2_SRC_DIR := $(BOOT_DIR)/stage2
 
 PREPROC_ASM := $(ROOT)/tools/preproc_asm.sh
 
-BOOT_SECT_SRCS := $(wildcard $(BOOT_SECT_DIR)/*.asm)
+BOOT_SECT_SRCS := $(shell find $(BOOT_SECT_DIR) -maxdepth 1 -type f -name '*.asm')
 BOOT_SECT_BINS := $(patsubst $(BOOT_SECT_DIR)/%.asm, $(BUILD_DIR)/boot/sector/%.bin, $(BOOT_SECT_SRCS))
 
 $(BUILD_DIR)/boot/sector/%.bin: $(BOOT_SECT_DIR)/%.asm $(PRELUDE)
 	$(call fasmg_compile,$<,$@,$(BOOT_SECT_DIR))
 
-STAGE2_C_SRCS := $(wildcard $(STAGE2_SRC_DIR)/*.c)
+STAGE2_C_SRCS := $(shell find $(STAGE2_SRC_DIR) -type f -name '*.c')
 STAGE2_S_OUTS := $(patsubst $(STAGE2_SRC_DIR)/%.c, $(BUILD_DIR)/boot/stage2/%.s, $(STAGE2_C_SRCS))
 STAGE2_D_DEPS := $(STAGE2_S_OUTS:.s=.d)
 
@@ -22,12 +22,12 @@ STAGE2_CFLAGS = $(CFLAGS) -I$(STAGE2_SRC_DIR)
 
 $(BUILD_DIR)/boot/stage2/%.s: $(STAGE2_SRC_DIR)/%.c $(PREPROC_ASM)
 	$(call mkdir_p,$(dir $@))
-	$(CC) $(STAGE2_CFLAGS) $(DEPFLAGS) -g0 -S -o - $< | sh $(PREPROC_ASM) > $@
+	set -o pipefail; $(CC) $(STAGE2_CFLAGS) $(DEPFLAGS) -g0 -S -o - $< | sh $(PREPROC_ASM) > $@
 
 $(STAGE2_C_ASM): $(STAGE2_S_OUTS)
 	$(call mkdir_p,$(dir $@))
 	@echo '; auto-generated - do not edit' > $@
-	$(foreach s,$(notdir $(STAGE2_S_OUTS)),@echo "include '$(s)'" >> $@;)
+	@$(foreach s,$(patsubst $(STAGE2_SRC_DIR)/%.c,%.s,$(STAGE2_C_SRCS)),echo "include '$(s)'" >> $@;)
 
 $(STAGE2_BIN): $(STAGE2_ASM) $(STAGE2_C_ASM) $(PRELUDE)
 	$(call fasmg_compile,$<,$@,$(abspath $(BUILD_DIR)/boot/stage2))
--- mk/cc.mk
+++ mk/cc.mk
@@ -2,9 +2,9 @@ CC := clang
 OBJCOPY ?= llvm-objcopy
 
 CC_TARGET := --target=i386-none-elf -std=gnu23
-CC_FSTAND := -ffreestanding -nostdlib -nostdinc -fno-builtin -masm=intel
+CC_FSTAND := -ffreestanding -nostdlib -fno-builtin -masm=intel
 CC_HARDEN := -fno-stack-protector -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables
-CC_CODEGEN  := -mno-sse -mno-sse2 -mno-mmx -mno-80387 -fno-pie -fno-pic
+CC_CODEGEN  := -march=i386 -mno-sse -mno-sse2 -mno-mmx -mno-80387 -fno-pie -fno-pic
 CC_WARNINGS := -Wall -Wextra -Wpedantic -Werror=implicit-function-declaration -Werror=return-type
 
 CC_MODE_FLAGS ?=
