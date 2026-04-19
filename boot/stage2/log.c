f32c7aed6d68140a6d5b826448dae0142f2c7e19
Yeint Herp <211648276+Yeint-herp@users.noreply.github.com>
2026-04-19 16:09:38 +0200

    feat(bootloader): Asserts

--- boot/stage2/log.c
+++ boot/stage2/log.c
@@ -8,6 +8,24 @@ void log_str(const char *s) {
         serial_puts(g_log, s);
 }
 
+void log_u32(uint32_t v) {
+    if (v == 0) {
+        log_str("0");
+        return;
+    }
+
+    char buf[11];
+    int i = 10;
+    buf[i--] = '\0';
+
+    while (v > 0) {
+        buf[i--] = (v % 10) + '0';
+        v /= 10;
+    }
+
+    log_str(&buf[i + 1]);
+}
+
 void log_hex32(uint32_t v) {
     static constexpr char hex[] = "0123456789abcdef";
 
@@ -22,4 +40,17 @@ void log_hex32(uint32_t v) {
 
 void log_set_sinker(SerialPort *port) {
     g_log = port;
+}
+
+void log_assert_fail(const char *expr, const char *file, uint32_t line) {
+    log_str("\r\n!!! ASSERTION FAILED: ");
+    log_str(expr);
+    log_str("\r\n!!! Location: ");
+    log_str(file);
+    log_str(":");
+    log_u32(line);
+    log_str("\r\n");
+
+    while (1)
+        __asm__ volatile("cli; hlt");
 }
