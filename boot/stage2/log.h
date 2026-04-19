aa5af3c9fd74b9f254eb3629631908a6f5490763
Yeint Herp <211648276+Yeint-herp@users.noreply.github.com>
2026-04-19 16:09:38 +0200

    feat(bootloader): Asserts

--- boot/stage2/log.h
+++ boot/stage2/log.h
@@ -6,6 +6,15 @@
 void log_set_sinker(SerialPort *port);
 
 void log_str(const char *s);
+void log_u32(uint32_t v);
 void log_hex32(uint32_t v);
+
+#define assert(cond)                                                                               \
+    do {                                                                                           \
+        if (!(cond))                                                                               \
+            log_assert_fail(#cond, __FILE__, __LINE__);                                            \
+    } while (0)
+
+void log_assert_fail(const char *expr, const char *file, uint32_t line);
 
 #endif /* STAGE2_LOG_H */
