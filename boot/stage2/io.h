2417334cbe2672fc1fe286d2bdc686e729987151
Yeint Herp <211648276+Yeint-herp@users.noreply.github.com>
2026-04-19 15:20:06 +0200

    fix: Fix fasmg preprocessing, local label scoping and inlining

--- boot/stage2/io.h
+++ boot/stage2/io.h
@@ -3,37 +3,37 @@
 
 #include <stdint.h>
 
-static inline void outb(uint16_t port, uint8_t val) {
+[[gnu::always_inline]] static inline void outb(uint16_t port, uint8_t val) {
     __asm__ volatile("out dx, al" : : "a"(val), "d"(port));
 }
 
-static inline uint8_t inb(uint16_t port) {
+[[gnu::always_inline]] static inline uint8_t inb(uint16_t port) {
     uint8_t val;
     __asm__ volatile("in al, dx" : "=a"(val) : "d"(port));
     return val;
 }
 
-static inline void outw(uint16_t port, uint16_t val) {
+[[gnu::always_inline]] static inline void outw(uint16_t port, uint16_t val) {
     __asm__ volatile("out dx, ax" : : "a"(val), "d"(port));
 }
 
-static inline uint16_t inw(uint16_t port) {
+[[gnu::always_inline]] static inline uint16_t inw(uint16_t port) {
     uint16_t val;
     __asm__ volatile("in ax, dx" : "=a"(val) : "d"(port));
     return val;
 }
 
-static inline void outl(uint16_t port, uint32_t val) {
+[[gnu::always_inline]] static inline void outl(uint16_t port, uint32_t val) {
     __asm__ volatile("out dx, eax" : : "a"(val), "d"(port));
 }
 
-static inline uint32_t inl(uint16_t port) {
+[[gnu::always_inline]] static inline uint32_t inl(uint16_t port) {
     uint32_t val;
     __asm__ volatile("in eax, dx" : "=a"(val) : "d"(port));
     return val;
 }
 
-static inline void io_wait(void) {
+[[gnu::always_inline]] static inline void io_wait(void) {
     outb(0x80, 0);
 }
 
--- include/prelude.asm
+++ include/prelude.asm
@@ -3,5 +3,10 @@ include 'fasm2/p5.asm'
 include 'fasm2/inlinemacro.asm'
 include 'fasm2/@@.asm'
 include 'fasm2/struct.asm'
+include 'fasm2/times.asm'
+
+macro pause
+    db 0xF3, 0x90
+end macro
 
 use16
